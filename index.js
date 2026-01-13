35ec1afbe0595fa5c86ff203b78ac920c46df5e8
sadmurror <nastya.rakina.00@bk.ru>
2026-01-13 17:52:17 +0500

    Last changes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("Всем привет")
+//git init
+//git add .
+//git commit -m "Init"
+//git branch -M main
+//git remote add origin "link"
+//git push -u origin main
+
+//При добавлении изменений в репозиторий
+//git add .
+//git commit -m "Changes"
+//git push -u origin main
